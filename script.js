18ceabc934ec5a28c905c8eba3559b7c579d8958
LIFLA MARY L K <124064755+liflamary@users.noreply.github.com>
2024-10-11 22:42:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const questions=[
+    {
+        question:'Which is the largest animal in the world?',
+        answers:[
+            {text:'Shark',correct:false},
+            {text:'Blue Whale',correct:true},
+            {text:'Elephant',correct:false},
+            {text:'giraffe',correct:false},
+        ]
+    },
+    {
+        question:'Which is the Smallest Country in the world?',
+        answers:[
+            {text:'Vatican City',correct:true},
+            {text:'Butan',correct:false},
+            {text:'Nepal',correct:false},
+            {text:'Sri Lanka',correct:false},
+        ]
+    },
+    {
+        question:'Which is the largest desert in the world?',
+        answers:[
+            {text:'Kalakari',correct:false},
+            {text:'Gobi',correct:false},
+            {text:'Sahara',correct:false},
+            {text:'Antartica',correct:true},
+        ]
+    },
+    {
+        question:'Which is the Smallest Continent in the world?',
+        answers:[
+            {text:'Asia',correct:false},
+            {text:'Astralia',correct:true},
+            {text:'Arctic',correct:false},
+            {text:'Africa',correct:false},
+        ]
+    }
+];
+const questionElement=document.getElementById('question');
+const answerButton=document.getElementById('answer-buttons');
+const nextButtons=document.getElementById('next-btn');
+let currentQuestionIndex=0;
+let score=0;
+function startQuiz(){
+    currentQuestionIndex=0;
+    score=0;
+    nextButtons.innerHTML='Next';
+    showQuestion();
+}
+function showQuestion(){
+    let currentQuestion=questions[currentQuestionIndex];
+    let questionNo=currentQuestionIndex+1;
+    questionElement.innerHTML=questionNo+"."+currentQuestion.question;
+    currentQuestion.answers.forEach(answer=>{
+        const button=document.createElement('button');
+        button.innerHTML=answer.text;
+        button.classList.add('btn');
+        answerButton.appendChild(button); 
+    })
+}
